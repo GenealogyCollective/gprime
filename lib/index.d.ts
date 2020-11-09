5de236342c764fb295556b6f53f621c84c83d65c
Douglas Blank <doug.blank@gmail.com>
2020-11-09 12:22:59 -0800

    Remove lib files from repo

--- lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { JupyterFrontEndPlugin } from '@jupyterlab/application';
-/**
- * Initialization data for the extension1 extension.
- */
-declare const extension: JupyterFrontEndPlugin<void>;
-export default extension;
